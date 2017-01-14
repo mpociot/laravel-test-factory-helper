ffed279484ef5a0ef499baebd6d441e537987820
Matt Campbell <matt.campbell@theshopworks.com>
2017-01-14 15:06:42 +0000

    Template: Remove extra spacing when generating properties
    Fixes #12

--- resources/views/factory.blade.php
+++ resources/views/factory.blade.php
@@ -2,7 +2,7 @@
 $factory->define({{$reflection->getName()}}::class, function (Faker\Generator $faker) {
     return [
 @foreach($properties as $name => $property)
-        '{{$name}}' => @if($property['faker']) {!!$property['type']!!} @else '{{$property['type']}}' @endif,
+        '{{$name}}' => @if($property['faker']){!!$property['type']!!}@else'{{$property['type']}}'@endif,
 @endforeach
     ];
 });
