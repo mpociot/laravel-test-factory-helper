a7c10898427fe232a12768635caf70cc384e9d99
skeets23 <skeets23@gmail.com>
2019-12-02 07:16:24 +0900

    Fix comments

--- src/Types/EnumType.php
+++ src/Types/EnumType.php
@@ -5,17 +5,20 @@ namespace Mpociot\LaravelTestFactoryHelper\Types;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 
-/**
- * My custom datatype.
- */
 class EnumType extends Type
 {
 
-    const ENUM = 'enum'; // modify to match your type name
+    const ENUM = 'enum';
 
     public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
     {
-        // this is wrong, but it also isn't important, since we're not modifying tables anyways. (this method must exist to match the abstract parent class, but we won't use it)
+        /* 
+         * This is wrong, but it also isn't important, since we're not modifying
+         * tables anyways. This method must exist to match the abstract parent
+         * class, but we won't use it. Normally, this should return the SQL 
+         * required to create an 'enum' column, but since this function doesn't
+         * know the values of the enum field, that's not possible.
+         */
         return 'ENUM';
     }
 
